e327bff10f8335cecc34d21a0090439ce2a6735f
Alvaro <alvar01omer@gmail.com>
2020-11-17 10:57:00 -0300

    some types examples

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 console.log("Hello!");
+var a = 10;
+var b = "adsad";
+var LanguagesKnown;
+(function (LanguagesKnown) {
+    LanguagesKnown[LanguagesKnown["English"] = 0] = "English";
+    LanguagesKnown[LanguagesKnown["Spanish"] = 1] = "Spanish";
+    LanguagesKnown[LanguagesKnown["Hindi"] = 2] = "Hindi";
+})(LanguagesKnown || (LanguagesKnown = {}));
+var student = {
+    Name: "Alvaro",
+    Age: 30,
+    Phone: 213546,
+    Language: LanguagesKnown.Hindi
+};
+var StudentsList = [
+    { Name: "Sara", Age: 26, Phone: 5465487987, Language: LanguagesKnown.English },
+    { Name: "Samantha", Age: 22, Phone: 5465487987, Language: LanguagesKnown.Spanish }
+];
+StudentsList.push(student);
+for (var index = 0; index < StudentsList.length; index++) {
+    console.log(StudentsList[index]);
+}
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,25 @@
 console.log("Hello!")
+
+let a:number = 10
+let b :String = "adsad"
+enum LanguagesKnown {English, Spanish, Hindi}
+
+let student = {
+    Name:"Alvaro",
+    Age: 30, 
+    Phone: 213546,
+    Language:LanguagesKnown.Hindi
+}
+
+let StudentsList = [
+    {Name:"Sara", Age:26, Phone:5465487987, Language: LanguagesKnown.English},
+    {Name:"Samantha", Age:22, Phone:5465487987, Language: LanguagesKnown.Spanish}    
+]
+
+StudentsList.push(student);
+
+for (let index = 0; index < StudentsList.length; index++) {
+    console.log(StudentsList[index])
+}
+
+
